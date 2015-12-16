d408d6760f42d13efe1f50fcd10f761d01a3567e
Frank Cass <frankcass1@gmail.com>
2015-12-16 01:31:34 -0800

    Example usage within append.sh updated

--- append.sh
+++ append.sh
@@ -7,11 +7,19 @@
 # 
 # Usage:       source append.sh
 # Alt Usage:   alias append="source /PATH/TO/append.sh"
-# Example:     $ cat command.log
+# Example:     
+#		$ echo "This is something important I want logged to remember later"
+#		$ append
+#		$ [*] Retrieving last command: echo "This is something important I want logged to remember later"
+# 		$ [*] Command appended to command.log: /Users/Frank/Desktop/command.log
+#		$ cat command.log
 # 			   $ Mon Dec 14 10:55:10 PST 2015: echo "This is something important I want logged to remember later"
+#
 # NOTE:        Must be sourced (Ex. $ source append.sh) to run properly, otherwise `history` will have nothing to report. See http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/x237.html
 #
 # Created and tested on OSX 10.11.1 (15B42) using iTerm2
+#
+###
 
 function last_command {
 history | tail -n 2 | head -n 1 | cut -d ' ' -f 4-80
