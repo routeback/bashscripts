00b61049275d50f3ac48f262bba6632496ed954c
routeback <frankcass1@gmail.com>
2015-12-18 23:18:49 -0800

    Troubleshooting lines 119 & 120

--- sslchecks.sh
+++ sslchecks.sh
@@ -112,17 +112,18 @@ function setnewtarget () {
 
 ### SSL Check functions below ###
 
-#function s_client_cert () {
-# echo | openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -issuer -subject -dates
-# echo | openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -text #Full output
-#}
-
 function supports_ssl () {
-status="openssl s_client -connect $ip:$port" # timeout 30s, if exit code failure then goto SSL/TLS not supported
-
-# How to check?
-# Perform a check if the server supports SSL/TLS connections or not. If it does not, can try to force (like nmap -PN)
-# If it does not support SSL/TLS, do not continue with the check, and call setnewtarget
+	# Perform a check if the server supports SSL/TLS connections or not. If it does not, provide the option to continue with the check anyway
+	# If it does not support SSL/TLS, do not continue with the check, and call setnewtarget, then return to main
+	echo "[*] Checking for SSL/TLS support..."
+	status="$(openssl s_client -connect $ip:$port)"
+	grep -q refused "$status"
+#       if [[ $status == *"Connection refused"* ]]
+#	if grep -q "Connection refused" <<<status=$(openssl s_client -connect $ip:$port); then # timeout 30s, if exit code failure then goto SSL/TLS not supported, timeout 30s $status
+#		no_ssl_support
+#	else
+#		echo "[*] Server appears to support SSL/TLS connections"
+#	fi
 }
 
 function no_ssl_support () {
@@ -132,7 +133,6 @@ setnewtarget
 
 function sslreneg () {
 echo "[*] -= SSL Renegotiation Checker =-"
-echo "[*] Checking for SSL/TLS support..."
 supports_ssl
 sslyze --reneg $ip:$port | tee $ip.$port.sslyze
 echo "[*] Saved as $ip.sslyze"
@@ -141,7 +141,6 @@ main
 
 function certinfo () {
 echo "[*] -= SSL Certificate Information =-"
-echo "[*] Checking for SSL/TLS support..."
 supports_ssl
 sslyze --certinfo=basic $ip:$port | tee $ip.certinfo # Ask for full or basic information?
 echo "[*] Certificate information saved as $ip.certinfo"
@@ -150,7 +149,6 @@ main
 
 function nmap_cert_info () {
 echo "[*] -= Nmap Certificate information scanner =-"
-echo "[*] Checking for SSL/TLS support..."
 supports_ssl
 nmap -p $port -sV --script=ssl-cert.nse $ip -oN $ip.nmap_certificate
 main
@@ -158,13 +156,17 @@ main
 
 function nmap_poodle () {
 echo "[*] -= Nmap SSL POODLE Scanner =-"
-echo "[*] Checking for SSL/TLS support..."
 supports_ssl
 nmap -p $port -sV --script=ssl-poodle.nse $ip -oN $ip.nmap_poodle # Add checking for ssl-poodle script
 # You do not have ssl-poodle.nse in your /usr/share/nmap/scripts folder. Download it with
 # wget -o ssl-poodle.nse <address>, then cp ssl-poodle.nse /usr/share/nmap/scripts; nmap --update-db # Then return to main
 main
 }
+
+#function s_client_cert () {
+# echo | openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -issuer -subject -dates
+# echo | openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -text #Full output
+#}
 
 target $1 $2
 main $1 $2
