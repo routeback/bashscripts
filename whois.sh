1e7fa998c3939db3e69caba8ec8008065eef604e
routeback <frankcass1@gmail.com>
2015-12-30 14:41:35 -0800

    Initial Upload. Basic functionality

---
+++ whois.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Name: whois.sh
+# Auth: Frank Cass
+# Date: 20151225
+# Desc: Quick WHOIS lookup script
+#
+###
+
+echo "[*] Enter organization name"
+read name
+echo "[*] Enter path to targets.ip"
+read targets
+echo "[*] Beginning WHOIS Lookup"; echo ""
+for i in $(cat $targets); do echo "WHOIS Lookup for: $i"; whois $i | grep $name; done
+echo "[*] Done"; echo ""
