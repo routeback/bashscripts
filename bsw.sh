5b194b3ef94275b2e63cb060c027f16d9122b2b8
routeback <frankcass1@gmail.com>
2016-01-19 14:25:10 -0800

    Initial commit

---
+++ bsw.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Name: bsw.sh
+# Auth: Frank Cass
+# Date: 20160119
+# Desc: Quick blacksheepwall usage interactive script and parsing
+#
+###
+
+echo "[*] Specify an input (e.g. targets.ip)"; read input
+echo "[*] Name the output file"; read output
+echo "[*] Provide a domain"; read domain
+echo "[*] Provide your Bing API key"; read bing
+echo "[*] Here we go..."
+( set -x; for i in $(cat targets.ip); do blacksheepwall -fcrdns -bing $bing -reverse -robtex -srv -domain $domain -axfr -headers $i -json >> $output.bsw.json; done )
+cat $output.bsw.json | sort -n | uniq | sort -n | uniq > z; cat z > $output.bsw.json; rm z;
+echo "[*] Done. Output saved as `pwd`/$output.bsw.json"
