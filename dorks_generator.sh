a23dd9374d97508035988da49c804e41d35f45e9
routeback <frankcass1@gmail.com>
2016-02-26 05:03:24 -0800

    Added GHDB URL, changed read input

--- dorks_generator.sh
+++ dorks_generator.sh
@@ -6,16 +6,14 @@
 # Desc: Generate a list of Google Dork syntax search terms for a given domain
 #
 # 	TODO: Find a way to automatically open all links after they have been created
+#		ex. for i in $(cat $baseurl.dorks); do chrome $i; done # Assumes default search is google and the user is running chrome
 # 	TODO: Or discover the Google url structure to automatically generate a bunch of .html links to just open with for i in $(cat dorks.html.list); do chrome $i; done
 #
 ###
 
 echo ""; echo "		-= dorks_generator.sh =-"
 echo "[*] Generate a list of Google Dork search terms for an organization"
-echo "[*] Enter domain to use for Google Dorking, (example.com)"
-read baseurl
-
-echo "[*] Generating Dorks list"
+read -p "[*] Enter domain to use for Google Dorking, (example.com): " baseurl
 cat << EOF >> $baseurl.dorks # Will prevent output of below echo's from being displayed; can then use column to print the output from the file
 [-] Search for directory listing vulnerabilities: 	site:$baseurl intitle:index.of
 [-] Search for configuration files: 			site:$baseurl ext:xml | ext:conf | ext:cnf | ext:reg | ext:inf | ext:rdp | ext:cfg | ext:txt | ext:ora | ext:ini
@@ -25,6 +23,7 @@ cat << EOF >> $baseurl.dorks # Will prevent output of below echo's from being di
 [-] Search for login pages: 				site:$baseurl inurl:login
 [-] Search for publicly exposed documents: 		site:$baseurl ext:doc | ext:docx | ext:odt | ext:pdf | ext:rtf | ext:sxw | ext:psw | ext:ppt | ext:pptx | ext:pps | ext:csv
 [-] Search for SQL errors: 				site:$baseurl intext:\"sql syntax near\" | intext:\"syntax error has occurred\" | intext:\"incorrect syntax near\" | intext:\"unexpected end of SQL command\" | intext:\"Warning: mysql_connect()\" | intext:\"Warning: mysql_query()\" | intext:\"Warning: pg_connect()\"
-EOF
-echo "[*] File saved as $baseurl.dorks"
 
+Need more dorks? https://www.exploit-db.com/google-hacking-database/
+EOF
+echo "[*] Done. File saved as $baseurl.dorks"
