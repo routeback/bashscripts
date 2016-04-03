a5ad1cbd1857450763db57d016f8d32a98312978
routeback <frankcass1@gmail.com>
2016-04-03 06:02:05 -0700

    Installation of CPAN req's for rdp-sec-check

--- dropper.sh
+++ dropper.sh
@@ -48,6 +48,8 @@ echo "[*] Installing iPwn Scripts"
 git clone https://github.com/altjx/ipwn
 echo "[*] Installing rdp-sec-check"
 git clone https://github.com/portcullislabs/rdp-sec-check.git
+perl -MCPAN -e "install Convert::BER"
+cpan install Encoding::BER
 echo "[*] Installing Lynis System Auditor"
 git clone https://github.com/CISOfy/lynis.git
 echo "[*] Installing CrackMapExec"
@@ -56,12 +58,11 @@ echo "[*] Installing Etherleak"
 wget -O etherleak.pl https://www.exploit-db.com/download/3555
 # Run ether leak, then separately hping3 -1 -d 0 1 <IP>
 echo "[*] Installing Javaws"
-apt-get install icedtea-netx # (javaws for launch.jnlp files)
+apt-get install icedtea-netx # (javaws for launch.jnlp files, such as when connecting to BMC interfaces)
 echo "[*] Installing SMBExec"
 git clone https://github.com/brav0hax/smbexec.git
 echo "[*] Installing Nmap script: vmware-fingerprint.nse"
 git clone https://gist.github.com/10695801.git; mv 10695801/vmware-fingerprint.nse /usr/share/nmap/scripts; rm -r 10695801
-# Potential Req and additional setup required:  perl -MCPAN -e "install Convert::BER"
 echo "[*] Installing Veil"
 git clone https://github.com/Veil-Framework/Veil
 cd Veil; ./Install.sh -c; echo "[*] Veil Setup Complete"
