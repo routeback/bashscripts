31942fe8df68fe8c558659718817d6e514468001
routeback <frankcass1@gmail.com>
2016-04-02 21:19:54 -0700

    Functionality improvements, Main menu button choices changed, read prompts changed

--- sslchecks.sh
+++ sslchecks.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-# set -x # Troubleshooting
 #
 # Name: sslchecks.sh
 # Auth: Frank Cass
-# Date: 20151219
+# Date: 20160412
 # Desc: TUI Interface for performing SSL checks of certificates and vulnerabilities
 #
-#	This script a WIP
-#	Todo: Implement all checks 1-8
-#	Todo: Implement checking for tool requirements before reaching main. See $require
-#	Todo: Save all files to ssl_checks folder
-#	Todo: Count total number of checks performed and echo at exit, pwd of ssl_checks
+#	Todo: Count total number of checks performed and echo at exit
 #	Todo: Implement ability to import a file, line delimeted $ip:$port - Specify full path
+#		Currently only supports single target manual IP entry. Want it to be flexible between single IP targets and performing checks against all hosts in a file and generating useful output
 # 	Todo: Main menu option to toggle saving output to files or to stdout only
 #		[F]ile Saving [On]/Off
-# 	Todo: Implement subshell with set -x enabled for each of the checks to print out the command that was used
-#	Todo: Implement main menu option for checking SSL/TLS support - goto supports_ssl (good for troubleshooting)
-#	Todo: Implement coloring of the vulnerable text from nmap or sslyze
-#	Goal: Try to keep it within 200 lines
+#	Thoughts: Should it be aware if a host has been previously checked? Ex. checking current directory for .sslchecks files regarding certificate info or other
+#	Thoughts: File output could be combined into a single file for all of the checks performed against a single host with regex for searching information regarding a host (Ex. press X for certificate expiry of this host)
 #
 ###
 
@@ -27,16 +21,15 @@
 count=0
 ip=$1
 port=$2
+fileoutput=On
+
 # require=curl,nmap,sslyze,openssl # Implement req. checks for all of the following
 				   # If they don't have curl or openssl, cannot proceed. Must have one
 				   # If they don't have nmap or sslyze, cannot proceed. Must have one
 				   # echo You do not meet the following requirements:
-
-# From SSLyze Github
-# SSLyze is all Python code but since version 0.7, it uses a custom OpenSSL wrapper written in C called nassl. The pre-compiled packages for SSLyze contain a compiled version of this wrapper in sslyze/nassl. If you want to clone the SSLyze repo, you will have to get a compiled version of nassl from one of the SSLyze packages and copy it to sslyze-master/nassl, in order to get SSLyze to run.
-#The source code for nassl is hosted at https://github.com/nabla-c0d3/nassl.
-
-
+				   # From SSLyze Github
+				   # SSLyze is all Python code but since version 0.7, it uses a custom OpenSSL wrapper written in C called nassl. The pre-compiled packages for SSLyze contain a compiled version of this wrapper in sslyze/nassl. If you want to clone the SSLyze repo, you will have to get a compiled version of nassl from one of the SSLyze packages and copy it to sslyze-master/nassl, in order to get SSLyze to run.
+				   #The source code for nassl is hosted at https://github.com/nabla-c0d3/nassl.
 
 function banner () {
 	clear
@@ -46,13 +39,13 @@ function banner () {
 	echo " ___) |__) | |__| |___|  _  | |__| |___| . \ ___) |\__ \ | | |"
 	echo "|____/____/|_____\____|_| |_|_____\____|_|\_\____(_)___/_| |_|"
 	echo ""
-	echo "       -= Quick SSL Checker TUI with file output =-"
+	echo "	-= Quick SSL/TLS Vulnerability Checker =-"
 	}
 
 function target () {
 	if [ $# -lt 2 ]
 	then
-		echo ""; echo "Usage: $0 IP Port"
+		echo "Usage: sslchecks.sh <IP> <Port>"
 		exit
 	else
 		currenttarget $1 $2 $count
@@ -60,50 +53,50 @@ function target () {
 }
 
 function check_counter () {
-#	if [ $count -gt 0 ]
-#	then
 		echo "[*] Previous task complete!"; echo ""
-		echo "[*] Return to main menu? [y/n]"; read yn; case $yn in
+		read -p "[*] Return to main menu? [y/n] " yn; case $yn in
 		[yY] | [yY][Ee][Ss] )
-			echo "[!] Back to main menu in 5..."; sleep 0.5;echo "[!] 4.."; sleep 0.5;echo "[!] 3.."; sleep 0.5;echo "[!] 2.."; sleep 0.5;echo "[!] 1.."; sleep 0.5;
 			;;
 		[nN] | [n|N][O|o] )
 			echo "[*] Exiting"; echo ""; exit
 			;;
 		*)
-			echo "[^_^] I do not understand. To the main menu we go!"; sleep 2
+			echo "[*] Returning to main menu"; sleep 2
 		esac
 		clear
-#	else
-#		count=`expr $count + 1`
-#	fi
 }
 
 function main () {
 banner
-echo ""; echo "[*] -= SSLChecks.sh Main Menu =-"
-echo "[*] Please select an SSL check to be performed"
-echo "[*] Current Target: [$ip:$port]"
-echo ""; echo "[!] NOTE: This script is still in development!"
+echo ""
+echo "[!] NOTE: This script is still in development!"
 echo ""; echo "Vulnerabilities:"
 echo "----1) SSLRenegotiation"
 echo "----2) SSL POODLE"
 echo "----3) LogJam"
 echo "----4) FREAK"
-echo ""; echo "Certificate Information"
-echo "----5) Certificate Information"
-echo "----6) Weak Hashing Algorithm"
 echo ""; echo "Ciphers:"
-echo "----7) All Ciphers"
-echo "----8) Insecure Only (SSLv2/3, RC4, Null, Export)"
+echo "----5) All Ciphers"
+echo "----6) Insecure Only (SSLv2/3, RC4, Null, Export, Anon)"
+echo ""; echo "Certificate Information"
+echo "----R) Retrieve Certificate Information"
+echo "----W) Weak Hashing Algorithm"
+echo "----X) Certificate Expiry"
 echo ""; echo "Options:"
 echo "----C) Change Target"
-echo "----F) Check for SSL/TLS support"
-echo "----X) Check Certificate Expiry"
+echo "----F) Toggle file saving: [$fileoutput]"
+echo "----S) Check for SSL/TLS support"
 echo "----Q) Quit"
-read sslcheck
+echo ""
+echo "[*] File output to: `pwd`"
+echo "[*] Current Target: [$ip:$port]"
+echo ""
+
+read -p "Please input a selection: " sslcheck
 case $sslcheck in
+
 # After selecting a choice, ex. FREAK or LogJam, give a brief description of the vulnerability
+
 		1)
 			sslreneg $1 $2
 			;;
@@ -117,35 +110,42 @@ case $sslcheck in
 			freak $1 $2
 			;;
 		5)
-			certificate_basic $1 $2
-			;;
-		6)
-			weak_hashing $1 $2
-			;;
-		7)
 			ciphers_full $1 $2
 			;;
-		8)
+		6)
 			ciphers_insecure  $1 $2
 			;;
-		9)
-			echo "[#] No option 9 available...Yet! What would you like to see here?"
-			echo "[#] Join in on the development here: https://github.com/routeback/bashscripts/blob/master/sslchecks.sh"
+		[rR])
+			certificate_basic $1 $2
+			;;
+		[wW])
+			weak_hashing $1 $2
 			;;
 		[qQ])
-			echo "[*] Exiting"; exit $?
-			;; # Also listten for keystroke Ctrl+C break
+			echo "[*] Exiting"; exit $? # Should this be return instead to prevent closing the terminal?
+			# Also listten for keystroke Ctrl+C break
+			;;
 		[cC])
 			setnewtarget
 			;;
-		[fF])
+		[fF])   # Toggle switch for file saving on / off
+#			if [ $fileoutput=true ]; then fileoutput=false
+#			else
+#			if fileoutput=false; then fileoutput=true; done; fi
+#			fi
+#			Change output path to NULL
+#			Off by default, if pressed, specify an output folder
+			main
+			;;
+		[sS])
 			supports_ssl $1 $2
 			;;
 		[xX])
 			certificate_expiry $1 $2
 			;;
 		*)
-			echo ""; echo "[!] Invalid Option"; sleep 1.5; main
+			echo ""; echo -n "[*] Invalid Option"; for i in $(seq 1 10); do echo -n "."; sleep 0.15; done
+			main
 			;;
 	esac
 }
@@ -166,7 +166,8 @@ function currenttarget () {
       			main
 	               	;;
 
-	        *) echo "Invalid input"
+	        *)
+			echo ""; echo -n "[*] Invalid Option"; for i in $(seq 1 10); do echo -n "."; sleep 0.15; done
         	        ;;
 		esac
 	else
@@ -175,9 +176,12 @@ function currenttarget () {
 }
 
 function setnewtarget () {
+	echo ""
 	echo "[*] Setting new target"
-	echo "[!] Please enter an IP or hostname"; read ip
-        echo "[!] Please enter a port"; read port
+	read -p "[*] Please enter an IP or hostname: " ip
+        read -p "[*] Please enter a port: " port
+	echo "[*] Target changed! Returning to main menu..."
+	sleep 2
 	main
 }
 
@@ -187,17 +191,13 @@ function supports_ssl () {
 	# Perform a check if the server supports SSL/TLS connections or not.
 	# If it does not, provide the option to change targets (setnewtarget)
 	# If targets are unchanged, provide the option to continue with the check anyway (force) or goto main menu
-	echo "[*] Checking for SSL/TLS support..."; echo ""
+	echo ""; echo "[*] Checking for SSL/TLS support..."; echo ""
 	ssl_connect="openssl s_client -connect $ip:$port 2> /dev/stdout" # ALT: curl --ssl-reqd -s -S https://$ip:$port | grep couldn't
 	timer="timeout 3s"
 	refused="$ssl | grep -q \"refused\""
 	accepted="$ssl | grep -i \"SSL handshake\""
-
 	echo "CURRENT EXIT CODE IS:" $? # Troubleshooting
-	echo "CHECKING SSL/TLS SUPPORT" # Troubleshooting
-
 	# $refused # Check for refused, if so then jump to no_ssl_support. If not refused, perform the check again looking for the SSL/TLS handshake
-
 	if [[ $? == 0 ]] # If grep exit code is 0 because it found Connection refused, then goto no_ssl_support
 	then
 		no_ssl_support
@@ -211,16 +211,17 @@ function supports_ssl () {
 # If supports_ssl is called from main menu and server does support SSL/TLS, does the script continue to the next function at no_ssl_support?
 
 function no_ssl_support () {
-echo ""; echo "[!] It appears this host does not support SSL/TLS connections."; echo ""
-echo "[*] Would you like to set a new target? [y/n]"; read yn; case $yn in
+	echo ""; echo "[!] It appears this host does not support SSL/TLS connections."; echo ""
+	read -p "[*] Would you like to set a new target? [y/n]: " yn; case $yn in
 	[yY] | [yY][Ee][Ss] )
 		setnewtarget
 		;;
 	[nN] | [n|N][O|o] )
+		echo ""
 		echo "[*] Target unchanged. Force SSL Check or return to Main Menu?"
 		echo "---[F]orce / [M]ain / [Q]uit"; read choice; case $choice in
 			[fF])
-				echo "[*] FORCE: Proceeding with SSL check"
+				echo "[!] This option does not currently work. Sleeping 3 seconds"; sleep 3
 				# How to force the check when the checks all begin with supports_ssl function?
 				# Shouldn't have to add any code for this, no_ssl_support should finish and then continue with the check
 				;;
@@ -234,91 +235,107 @@ echo "[*] Would you like to set a new target? [y/n]"; read yn; case $yn in
 			esac
 		;;
 	*)
-		echo "[*] I don't understand!" # What happens after this? 
+		echo ""; echo -n "[*] Invalid Option"; for i in $(seq 1 10); do echo -n "."; sleep 0.15; done
 		;;
 	esac
 }
 
-# Menu option #1
 function sslreneg () {
-clear
-echo ""; echo "[*] -= SSL Renegotiation Checker =-"
-supports_ssl
-( set -x; sslyze --reneg $ip:$port | tee $ip.$port.sslyze )
-echo "[*] Saved as $ip.sslyze"
-check_counter
+	clear
+	echo ""; echo "[*] -= SSL Renegotiation Checker =-"
+	supports_ssl
+	( set -x; sslyze --reneg $ip:$port | tee $ip.$port.sslyze )
+	echo "[*] Saved as $ip.sslyze"
+	check_counter
 }
 
-# Menu option #2
 function nmap_poodle () {
-clear
-echo "[*] -= Nmap SSL POODLE Scanner =-"
-supports_ssl
-( set -x; nmap -Pn -p $port -sV --script=ssl-poodle.nse $ip -oN $ip.nmap_poodle )
-echo ""; echo "[*] Saved as $ip.nmap_poodle"
-# Add checking for ssl-poodle script | while grep nmap if stderr throws nscript not found, goto ssl-poodle.nse_missing ()
-# You do not have ssl-poodle.nse in your /usr/share/nmap/scripts folder. (Would you like to) Download it with
-# wget -o ssl-poodle.nse <address>, then cp ssl-poodle.nse /usr/share/nmap/scripts; nmap --update-db # Then return to main
-check_counter
+	clear
+	echo "[*] -= SSL/TLS POODLE Vulnerability Check =-"
+	supports_ssl
+	( set -x; nmap -Pn -p $port -sV --script=ssl-poodle.nse $ip -oN $ip.nmap_poodle )
+	echo ""; echo "[*] Saved as $ip.nmap_poodle"
+	# Add checking for ssl-poodle script | while grep nmap if stderr throws nscript not found, goto ssl-poodle.nse_missing ()
+	# You do not have ssl-poodle.nse in your /usr/share/nmap/scripts folder. (Would you like to) Download it with
+	# wget -o ssl-poodle.nse <address>, then cp ssl-poodle.nse /usr/share/nmap/scripts; nmap --update-db # Then return to main
+	check_counter
 }
 
-# Menu option #3
 function logjam () {
-clear
-echo ""; echo "[*] -= SSL/TLS LogJam Check =-"
-supports_ssl
-( set -x; openssl s_client -connect $ip:$port -cipher "EXP" | tee $ip.logjam )
-echo ""; echo "[*] Saved as $ip.logjam"
-check_counter
+	clear
+	echo ""; echo "[*] -= SSL/TLS LogJam Check =-"
+	supports_ssl
+	( set -x; openssl s_client -connect $ip:$port -cipher "EXP" | tee $ip.logjam )
+	echo ""; echo "[*] Saved as $ip.logjam"
+	check_counter
 }
 
-# Menu option #4
 #function freak () {
 # ( set -x; )
 #}
 
 # Menu option #5 - To be combined with number 6 to automatically check for the weak hashing algorithm
 function certinfo () {
-clear
-echo ""; echo "[*] -= SSL Certificate Information =-"
-supports_ssl
-( set -x; sslyze --certinfo=basic $ip:$port | tee $ip.certinfo ) # Ask for full or basic information?
-echo "[*] Certificate information saved as $ip.certinfo"
-check_counter
+	clear
+	echo ""; echo "[*] -= SSL Certificate Information =-"
+	supports_ssl
+	( set -x; sslyze --certinfo=basic $ip:$port | tee $ip.certinfo ) # Ask for full or basic information?
+	echo "[*] Certificate information saved as $ip.certinfo"
+	check_counter
 }
 
 # Menu option #n - To replace 6 as an alternate certificate information grabber
 function nmap_cert_info () {
-clear
-echo ""; echo "[*] -= Nmap Certificate information scanner =-"
-supports_ssl
-( set -x; nmap -p $port -sV --script=ssl-cert.nse $ip -oN $ip.nmap_certificate )
-check_counter
+	clear
+	echo ""; echo "[*] -= Nmap Certificate information scanner =-"
+	supports_ssl
+	( set -x; nmap -p $port -sV --script=ssl-cert.nse $ip -oN $ip.nmap_certificate )
+	check_counter
 }
 
-# Menu option #7
 function ciphers () {
-supports_ssl
-# ( set -x; )
-check_counter
+	supports_ssl
+	# ( set -x; )
+	check_counter
 }
 
-# Menu option #8
 # Check for CRIME, FREAK, LogJam, Export, Heartbleed
 function insecure_ciphers () {
-supports_ssl
-# ( set -x; )
-check_counter
+	supports_ssl
+	# ( set -x; )
+	check_counter
 }
 
 function certificate_expiry () {
-supports_ssl
-# Could implement another small TUI for what kind of checks
-# openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -issuer -subject -dates
-# Other openssl s_client options: -purpose, -ignore_critical, -issuer_checks, -crl_check, -crl_check_all, -policy_check, -extended_crl, -x509_strict, -policy -check_ss_sig
-( set -x; timeout 3s openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -text | grep -i -A 1 "Not Before" )
-check_counter
+	supports_ssl
+	# Could implement another small TUI for what kind of checks
+	# openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -issuer -subject -dates
+	# Other openssl s_client options: -purpose, -ignore_critical, -issuer_checks, -crl_check, -crl_check_all, -policy_check, -extended_crl, -x509_strict, -policy -check_ss_sig
+	( set -x; timeout 3s openssl s_client -connect $ip:$port 2>/dev/null | openssl x509 -noout -text | grep -i -A 1 "Not Before" )
+	check_counter
 }
 
 target $1 $2
 main $1 $2
+
+# NEW CHECKS
+
+#MS14_066_CIPHERS="DHE-RSA-AES256-GCM-SHA384 DHE-RSA-AES128-GCM-SHA256 AES256-GCM-SHA384 AES128-GCM-SHA256"
+# Ciphers supported by Windows Server 2012R2
+#WINDOWS_SERVER_2012R2_CIPHERS="ECDHE-RSA-AES256-SHA384 ECDHE-RSA-AES256-SHA"
+
+# Test if OpenSSL does support the ciphers we're checking for...
+#echo -n "Testing if OpenSSL supports the ciphers we are checking for: "
+#openssl_ciphers=$(openssl ciphers)
+
+#MS14_066 Ciphers
+#
+#for c in $MS14_066_CIPHERS
+#do
+#  if ! echo $openssl_ciphers | grep -q $c 2>&1 >/dev/null
+#  then
+#    echo -e "\033[91mNO (OpenSSL does not support $c cipher.)\033[0m"
+#    echo -e "\033[91mAborting."
+#    exit 5
+#  fi
+
