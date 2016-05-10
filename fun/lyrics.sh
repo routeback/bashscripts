d3191d385ee8281abd35220f1bfe34e7e0ab26e1
routeback <frankcass1@gmail.com>
2016-05-10 15:27:14 -0700

    Additional functionality - Better error handling, ability to save lyrics to file, sanity checks

--- fun/lyrics.sh
+++ fun/lyrics.sh
@@ -2,23 +2,27 @@
 #
 # Name: lyrics.sh
 # Auth: Frank Cass
-# Date: 20160428
+# Date: 20160511
 # Desc: Query for a songs lyrics and display them to the user.
 #       Works best when input is <Band> <Song Name>
 #
 ###
 
+# Flush temporary saved files
+
+rm /tmp/lyric_curl.html > /dev/null 2>&1; rm /tmp/lyrics.html > /dev/null 2>&1
+
 # Banner
 
 function ascii_art () {
 echo ""
-echo "|___|)___________________________________________________________"
+echo " ___|)___________________________________________________________"
 echo "|___/____________________________________________________________"
 echo "|__/|____________________________________________________________"
 echo "|_/(|,\___Lyrics.sh______________________________________________"
 echo "|_\_|_/__________________________________________________________"
-echo "|   | "
-echo "| (_|   Enter <Band> <Song Name> to begin"
+echo "    | "
+echo "  (_|   Enter <Band> <Song Name> to begin"
 echo
 }
 
@@ -37,27 +41,62 @@ baseurl=http://search.plyrics.com/search.php?q=
 edited_url=$(echo $baseurl | sed "s/=/=$search_string=/g")
 }
 
-function urlquery () {
-
 # Perform the search for lyrics with the search query
 
-curl -s -o /tmp/lyric_curl.html $edited_url
-
+function urlquery () {
+curl -s -o /tmp/lyric_curl.html $edited_url 2> /dev/null
 first_result=$(cat /tmp/lyric_curl.html | grep -A 1 "<td><b>1" | grep -i "<a href=" | cut -d '"' -f 2)
 
 # Make the curl request to the lyric website for the search results and save to a file
 
-curl -s -o /tmp/lyrics.html -A "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5" $first_result
+curl -s -o /tmp/lyrics.html -A "Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5" $first_result 2> /dev/null
+}
 
-# Formatting text output
+# Check if any results were returned or not (exit), if true (display lyrics)
 
-echo "[*] Found Song Lyrics!"; echo
+function printlyrics () {
+if [ ! -s /tmp/lyrics.html ]
+	then
+	echo "[*] No lyrics found :("; exit 0
+
+# TODO: If no lyrics found, search another site or query some lyric API (potential re-write)
+
+fi
+
+# Sometimes it returns .html that is not lyrics (in /tmp/lyrics.html)
+# If this happens, check for an identifier (submit corrections) that the page actually contains lyrics
+# Potential TODO: This non-lyric containing .html page could be used to provide recommendations for other related songs by grepping for "/lyrics..." in href's if $search is not found
+
+if grep -qi "Submit Corrections" /tmp/lyrics.html; then
+echo "[*] Found Song Lyrics!"; echo ""
 cat /tmp/lyrics.html | sed -n '/start of lyrics/,/end of lyrics\|^$\|pattern/p' | grep -B 100 "end of lyrics" | grep -v "start of lyrics" | grep -v "end of lyrics" | sed 's#<br />##g' | grep -v "<i>"; echo
 echo "[*] End Song Lyrics"; echo
+else
+echo "[*] No lyrics found :("; exit 0
+fi
+}
 
-# Remove temporary saved files
+function savelyrics () {
+read -p  "[*] Would you like to save these lyrics to a file: [Y/N] " answer
+	case $answer in
+	[yY] | [yY][Ee][Ss] )
+	      	read -p "[*] Enter a filename: [Ex. Band_Song.lyrics] " savename
+		echo "[*] Saving lyrics to: `pwd`/$savename"
+		touch $savename
+		cat /tmp/lyrics.html | sed -n '/start of lyrics/,/end of lyrics\|^$\|pattern/p' | grep -B 100 "end of lyrics" | grep -v "start of lyrics" | grep -v "end of lyrics" | sed 's#<br />##g' | grep -v "<i>" >> `pwd`/$savename
+               	;;
 
-rm /tmp/lyric_curl.html > /dev/null 2>&1; rm /tmp/lyrics.html > /dev/null 2>&1
+        [nN] | [n|N][O|o] )
+		;;
+	*)
+		echo "[*} Invalid input"
+		exit 0
+       	        ;;
+	esac
+}
+
+function goodbye () {
+	echo ""; echo "	\"Music can change the world, because it can change people.\" - Bono"; echo ""
 }
 
 # Call all of the functions
@@ -65,3 +104,6 @@ rm /tmp/lyric_curl.html > /dev/null 2>&1; rm /tmp/lyrics.html > /dev/null 2>&1
 ascii_art
 query
 urlquery
+printlyrics
+savelyrics
+goodbye
