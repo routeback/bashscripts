7942e0571b6f7ec8ef55d550ba50abf2dd173efb
Frank Cass <frankcass1@gmail.com>
2017-09-17 13:35:07 -0400

    Initial Commit

---
+++ docker/lair.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Name: lair.sh
+# Auth: Frank Cass
+# Date: 20170917
+# Desc: Starts up the lair docker container pulled from Docker Hub.
+# Reference: https://hub.docker.com/r/routeback/starboard/tags/
+#
+# Usage:
+# 	$ docker pull routeback/starboard:lair-backup
+#	$ docker images
+#	$ docker tag <imageID> lair-backup:lair
+#	$ lair.sh
+#
+###
+
+docker run -d -it -v ~/docker/lairdata:/root/lairdata -p 11013:11013 lair:fresh /bin/bash -c "cd /root/lair; ./lairstart.sh"
+
+# -d daemon
+# -it interactive
+# -p 11013:11013 port forward
+# -v attached data volume for persistence
