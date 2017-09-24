7259aecb6f535e23158dfa9e49343341e1261303
Frank Cass <frankcass1@gmail.com>
2017-09-23 23:27:21 -0400

    Made install.sh interactive for clean or cach building when making changes to the Dockerfile

--- docker/securityonion/install.sh
+++ docker/securityonion/install.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
-docker build --no-cache -t securityonion .
+
+# Build the docker image from the Dockerfile
+
+function buildtype () {
+	echo "[*] Docker Security Onion Install Script"
+	read -p "[*] Would you like a clean build [1] or or a cache build [2]: " a; case $a in
+	[1] )
+		docker build --no-cache -t securityonion .
+		;;
+	[2] )
+		docker build -t securityonion .
+		;;
+	*)
+		echo -n "[*] Invalid Option"; for i in $(seq 1 5); do echo -n "!"; sleep 0.05; done; echo ""
+		;;
+	esac
+}
+
+buildtype
+
