016d91eb8c1ae0c146cb34971922d6a6ea89d9f5
root <root@localhost.localdomain>
2018-01-02 09:23:06 -0500

    Initial Commit

---
+++ remote_testing_appliance/vpn_monitor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Name: vpn_monitor.sh
+# Auth: Frank Cass
+# Date: 20170101
+# Desc: Monitor the status of the openvpn process to determine if a vpn connection is established or not.
+#	Combine with Conky for a visual desktop widget.
+#
+###
+
+sleep_seconds=1
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+vpn_check() {
+
+if pidof openvpn > /dev/null is true
+	then
+		echo -e "${GREEN}[*] OpenVPN is active."
+		sleep $sleep_seconds
+		vpn_check
+	else
+		echo -e "${RED}[!] VPN is not connected."
+		sleep $sleep_seconds
+		vpn_check
+	fi
+}
+
+vpn_check
