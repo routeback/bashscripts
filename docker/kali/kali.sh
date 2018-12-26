4936481ba5195e4035740ee6345334c8609114b5
routeback <frankcass1@gmail.com>
2018-12-26 17:51:06 -0500

    Additional instructions added

--- docker/kali/kali.sh
+++ docker/kali/kali.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
 # Start customized Kali docker image interactively
-
-docker run --privileged -it -v ~/docker/kalidata:/root/kalidata kali4:security
-
+# First time setup:
+# docker pull kalilinux/kali-linux-docker
+# Create a folder for sharing files with the container:
+# mkdir -p ~/docker/kalidata
+docker run --privileged -it -v ~/docker/kalidata:/root/kalidata kalilinux/kali-linux-docker:latest
