d621a517825c1b9fa046730ea1c89449135946cf
routeback <frankcass1@gmail.com>
2019-03-26 14:09:59 -0400

    Added prompting for port, re-structured prompts

--- amazon_web_services/manualupdate.sh
+++ amazon_web_services/manualupdate.sh
@@ -2,17 +2,19 @@
 #
 # Name: secgroup_update.sh
 # Auth: Frank Cass
-# Date: 20170613
+# Date: 20190326
 # Desc: Quickly update your EC2 securitygroup with your public IP address to allow for SSH access
 #
 ###
 
-echo "[*] $0 - Amazon AWS Security Group Ingress SSH Updater"
+clear
+echo "[*] Amazon AWS Security Group Ingress Port Updater"
+read -p "[*] Enter a port you would like to whitelist: " port
 checkpublic=$(pub=$(curl -s ifconfig.co); echo "$pub" | awk '{ print $0 "/32" }')
-echo "[*] I think your public IP is: $checkpublic - But you should double check."
 read -p "[*] Enter the name of the security group for updating: " group
-read -p "[*] Enter public IP to be added (xxx.xxx.xxx.xxx/32): " ip
-( set -x; aws ec2 authorize-security-group-ingress --group-name $group --protocol tcp --port 22 --cidr $ip )
+echo "[*] Your public IP is: $checkpublic"
+read -p "[*] Enter public IP to be added to the security group (xxx.xxx.xxx.xxx/32): " ip
+( set -x; aws ec2 authorize-security-group-ingress --group-name $group --protocol tcp --port $port --cidr $ip )
 echo "[*] Done."
 
 
