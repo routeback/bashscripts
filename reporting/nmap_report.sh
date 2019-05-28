60832c8938d9ba2367e1e279413da5e42e29bcc1
routeback <frankcass1@gmail.com>
2019-05-28 15:16:42 -0400

    Positional parameter functionality WIP

--- reporting/nmap_report.sh
+++ reporting/nmap_report.sh
@@ -4,38 +4,61 @@
 # Auth: Frank Cass
 # Date: 20190525
 # Desc: This script will check for XML files in the current directory and generate an nmap HTML report.
+# 	Additionally, this script can be used to merge multiple scans or generate multiple reports.
 #
 # Reference: https://nmap.org/book/output-formats-output-to-html.html
+#
+# TODO: Expand capability to detect what type of XML file is being parsed and support additional tool exports.
 #
 ###
 
 echo "[*] Nmap HTML Report Generator"
 
+usage (){
+				echo "[*] Description: Quickly generate an nmap HTML report."
+				echo "[*] OPTIONAL Parameters:"
+				echo "		--all"
+				echo "			Generate a report for each .XML file in the current directory"
+				echo "		--merge"
+				echo "			For all nmap.xml files, merge them into a single .XML generate a single report"
+				echo "		--alias"
+				echo "			Add the alias 'nreport' to your ~/.bashrc "
+				echo "		-h (--help)"
+				echo "			 This help menu"
+}
+
 if [[ "$#" -gt 1 ]]; then
-	echo "[*] EXIT: One parameter at a time, please!"
-	echo "[*] Try: $0 -h"
+	usage
+	echo ""; echo "[*] EXIT: One parameter at a time, please!"
 	exit 1
 fi
 
 while [ "$1" != "" ]; do
     case $1 in
-        -a | --all )	# For every .xml in the current directory, run the report generator against it
+        --all )	# For every .xml in the current directory, run the report generator against it
+				echo "[*] Generating a report for each XML in the current directory."
+				# Implement loop function here
+				exit 0 # Remove this when loop implemented
                                 ;;
-        -m | --merge )	# For all nmap.xml files, merge them into a single .XML and jump straight to the report generation function
+        --merge )	# For all nmap.xml files, merge them into a single .XML and jump straight to the report generation function
+				echo "[*] Merging all XML files into a single report."
+				# Implement merge function here
+				exit 0 # Remove this when merge implemented
                                 ;;
-        -a | --alias )	# Add the alias 'nreport' to your ~/.bashrc file, echo "alias nreport='$0'" >> ~/.bashrc
+        --alias )	# Add the alias 'nreport' to your ~/.bashrc file
+				echo "[*] Adding alias 'nreport' to your ~/.bashrc."
+				echo "alias nreport='`pwd`/$0'" >> ~/.bashrc
+				exit 0
 				;;
 	-h | --help )	# This help menu of usage instructions
+				usage
+				exit 0
 				;;
-        * )                     echo "[*] EXIT: Invalid parameter supplied."
-				echo "[*] Try: $0 -h"
+        * )                     usage
+				echo ""; echo "[*] EXIT: Invalid parameter supplied."
                                 exit 1
     esac
 done
-
-# usage () {
-# [OPTIONAL]
-# }
 
 if [ $(ls | grep .xml | wc -l) -eq "0" ]; then # Check if any XML files are in the directory
 	echo "[!] No nmap XML files found in this directory!"
