203f27f89eecc07de1812e2cb58245e85ca28b71
routeback <null>
2020-02-05 22:20:24 -0500

    Updated

--- configs/linux/ipv6_toggle.sh
+++ configs/linux/ipv6_toggle.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 echo "[*] IPv6 Support Toggler"
-nano /etc/sysctl.conf
-#disable ipv6 = 1, Enable = 0 
+echo "[*] Edit the following file: /etc/sysctl.conf"
+echo "[*] Change the following lines as necessary."
+echo "[*] Disable ipv6 = 1"
+echo "[*] Enable ipv6 = 0"
+
+echo "`cat <<EOF
+
 net.ipv6.conf.all.disable_ipv6 = 0
 net.ipv6.conf.default.disable_ipv6 = 0
 net.ipv6.conf.lo.disable_ipv6 = 0
 net.ipv6.conf.eth0.disable_ipv6 = 0
+EOF`"
+echo ""
+echo "[*] Then enable new settings with: sysctl -p"
+echo "[*] Verify with: cat /proc/sys/net/ipv6/conf/all/disable_ipv6"
+echo "`cat <<EOF
 
-# Enable new settings
-sysctl -p
-
-$ cat /proc/sys/net/ipv6/conf/all/disable_ipv6
+[*] Alternative method to enable/disable:
 
 sysctl net.ipv6.conf.all.disable_ipv6=0
 sysctl net.ipv6.conf.all.disable_ipv6=1
+EOF`"
